8d7c99b52e8ebece7b2e467662b9e63ea4f92304
LENOVO <24130258@st.hcmuaf.edu.vn>
2026-05-15 17:13:32 +0700

    update matrix

--- src/demo.java
+++ src/demo.java
@@ -1,7 +1,19 @@
 public class demo {
-    static void main() {
-        System.out.println("Hello world");
-        System.out.println("update");
-        System.out.println("1");
+    static void main(String[] args) {
+        // Create a 3x3 matrix
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Print the matrix
+        System.out.println("Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
